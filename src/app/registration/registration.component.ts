5aa5e4ac98c2a9d473e33c85d06271c8e1e6f6d9
Nemanja1996S <savic1996.neca@gmail.com>
2024-02-26 16:13:26 +0100

    Registration component & validators

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,12 +1,35 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'registration',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
 export class RegistrationComponent {
 
+  visiblePassword : boolean = false;
+
+  registryFormGroup = new FormGroup({
+    nameFormControl: new FormControl('', Validators.required),
+    surnameFormControl: new FormControl('', Validators.required),
+    emailFormControl: new FormControl('', [Validators.required, Validators.email]),
+    passwordFormControl: new FormControl('', Validators.required),
+    dateFormControl: new FormControl(''),
+    educationFormControl: new FormControl(''),
+    workFormControl: new FormControl(''),
+    aboutMeFormControl: new FormControl(''),
+  });
+
+  submitForm(){
+    console.log(this.registryFormGroup.value);
+  }
+
+  togglePasswordText(){
+    this.visiblePassword = !this.visiblePassword;
+    
+  }
 }
