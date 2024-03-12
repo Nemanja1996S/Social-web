6fd00539c069a67a910b1685aa5218f21cb0ca60
Nemanja1996S <savic1996.neca@gmail.com>
2024-03-12 01:02:00 +0100

    Registation change

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,23 +1,27 @@
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'registration',
   standalone: true,
-  imports: [ReactiveFormsModule, NgIf],
+  imports: [ReactiveFormsModule, NgIf, NgFor],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
 export class RegistrationComponent {
 
   visiblePassword : boolean = false;
+  sportsOptions = ["Soccer", "Basketball", "Tennis"] ;
+  sportsSelected: string[] = [];
 
   registryFormGroup = new FormGroup({
     nameFormControl: new FormControl('', Validators.required),
     surnameFormControl: new FormControl('', Validators.required),
     emailFormControl: new FormControl('', [Validators.required, Validators.email]),
     passwordFormControl: new FormControl('', Validators.required),
+    //sportsArrayFormControl: new FormControl([], Validators.required),
+    selectedSportFormControl: new FormControl('', Validators.required),
     dateFormControl: new FormControl(''),
     educationFormControl: new FormControl(''),
     workFormControl: new FormControl(''),
@@ -31,5 +35,17 @@ export class RegistrationComponent {
   togglePasswordText(){
     this.visiblePassword = !this.visiblePassword;
     
+  }
+
+  addSelectedOption(){
+    //this.sportsSelected.push(option);
+    const chosenSport = this.registryFormGroup.get('selectedSportFormControl')?.value ?? '';
+    if(chosenSport){
+      if(!this.sportsSelected.find( (sport) => sport === chosenSport)){
+        this.sportsSelected.push(chosenSport)
+      }
+      
+    }
+    console.log(this.sportsSelected);
   }
 }
