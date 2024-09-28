5e7c48476d9b0f4f7d73cd563f017a0c274eadfd
Nemanja1996S <savic1996.neca@gmail.com>
2024-09-28 23:46:05 +0200

    Mini upgrade

--- src/app/person-profile/person-profile.component.ts
+++ src/app/person-profile/person-profile.component.ts
@@ -7,6 +7,10 @@ import {MatListModule} from '@angular/material/list';
 import { MatIcon } from '@angular/material/icon';
 import { MatCard } from '@angular/material/card';
 import { MatButton } from '@angular/material/button';
+import { Store } from '@ngrx/store';
+import { AppState } from '../store/app.state';
+import { Observable, of } from 'rxjs';
+import { userSelector } from '../store/user/user.selector';
 
 @Component({
   selector: 'person-profile',
@@ -18,11 +22,20 @@ import { MatButton } from '@angular/material/button';
 export class PersonProfileComponent implements OnInit{
 
   user: User = initialUser;
-  constructor(){
+  loggedInUser : Observable<User> = of()
+  isSelfProfile : boolean = false;
+
+  constructor(private store: Store<AppState>){
     // console.log(this.router.getCurrentNavigation()?.extras.state)
   }
   ngOnInit(): void {
     this.user = history.state;
+    this.loggedInUser = this.store.select(userSelector);
+    this.loggedInUser.subscribe((loggedUser) => {if(loggedUser.id === this.user.id) {console.log("Before changing"+this.isSelfProfile); this.isSelfProfile = true; console.log("After changing"+this.isSelfProfile)}})
+
   }
 
+  isMe(){
+    console.log(this.isSelfProfile)
+  }
 }
